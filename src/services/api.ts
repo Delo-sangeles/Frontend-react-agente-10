eb17064b288328b2ae3ae19068bcf75cfdfac8d8
Naizabeth <naiza1996n@gmail.com>
2026-06-14 17:32:15 +0200

    add .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 // services/api.ts
 
-const API_URL = "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 const MOCK = false; // cambia a false cuando el backend esté listo
 
